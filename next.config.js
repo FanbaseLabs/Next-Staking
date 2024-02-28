3d1edaeb254e781dc6d63cdc1bf03f90cd5beabd
FanboyLives <fanboydeveloper@gmail.com>
2024-02-28 21:22:00 +0900

    repository initialized

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  transpilePackages: ["react-leaflet-cluster"],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+  webpack(config, { isServer }) {
+    if(!isServer) {
+      config.resolve.fallback = { 
+        net: false,
+        tls: false,
+        fs: false
+      };
+    }
+    
+    return config
+  }
 }
